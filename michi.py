b965daa98108eecd1a730d6316aa8655e0d80880
YaelUribe <yael3x@gmail.com>
2022-03-18 23:33:26 -0500

    added documentation

--- michi.py
+++ michi.py
@@ -4,12 +4,15 @@ from models.base_module import MichiBot
 from models import resources
 import os
 import requests
-
+"""Flask app to interact with the user trhough Twilio"""
 app = Flask(__name__)
 
 
 @app.route('/bot', methods=['POST'])
 def bot():
+    """Endpoint /bot
+    & method to interpretate user incomming messages to generate a code url
+    to retrieve the desired REA"""
     incoming_msg = request.values.get('Body', '')
     resp = MessagingResponse()
     msg = resp.message()
@@ -31,6 +34,8 @@ def bot():
 
 @app.route('/<int:n>', strict_slashes=False)
 def resource(n):
+    """Endpoint /<int:n>. Method resource useful to return the right file
+    based on the incoming code(int)"""
     print("incoming code:", n)
     media = resources.files(n)
     print(media)
